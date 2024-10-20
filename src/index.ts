c6a83e61fddf7fc540b83174bc9271f3b6c752e7
Austin Cripe <aaustinc@live.com>
2024-10-19 20:30:46 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
 // src/index.ts
 import express from 'express';
+import axios from 'axios'
 
 const app: express.Application = express();
 const port = 3000;
 
-app.use(express.text());
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`server is listening on ${port}`);
 });
 
 // Homepage
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.status(200).send("Hello World!");
+app.get('/', async (req: express.Request, res: express.Response) => {
+  
+  const headers = { headers: {"X-Auth-Token": '2hR8gY9yPmEC0PkB8ScnE19EdvcEannf4xWK'} }
+
+  // returns array of objects
+  const response = await axios.get('https://ssav.flexrentalsolutions.com/f5/api/scan-log/scan-history?page=0&size=20&sort=scanDate%2Cdesc', headers)
+  
+  res.status(200).send(response.data.content) 
 });
 
 // GET
